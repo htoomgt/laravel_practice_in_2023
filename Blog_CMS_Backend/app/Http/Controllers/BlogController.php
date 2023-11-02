df6145107d437bdc60813d00cba9a6d73cd0aafc
Htoo Maung Thait <hmthait@blueplanetmm.com>
2023-11-03 01:39:48 +0630

    blog controller dev init

--- Blog_CMS_Backend/app/Http/Controllers/BlogController.php
+++ Blog_CMS_Backend/app/Http/Controllers/BlogController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\BlogRepository;
 
-class BlogController extends Controller
+class BlogController extends BaseController
 {
-    //
+    private $blogRepository;
+
+    public function __construct(BlogRepository $blogRepository)
+    {
+        parent::__construct();
+
+        $this->blogRepository = $blogRepository;
+    }
+
+    public function index(Request $request) {}
+
+    public function store(Request $request) {}
+
+    public function updateById(Request $request, $id) {}
+
+    public function deleteById(Request $request, $id) {}
+
+    public function getById(Request $request, $id) {}
+
+    public function customSearch(Request $request) {}
+}
 }
